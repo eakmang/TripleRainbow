746e71a5f46f6d1e4c5382b75084101fb4123300
Michael Pratt <urthen@gmail.com>
2012-03-07 20:40:06 -0600

    Popin created

--- js/triplerainbow.js
+++ js/triplerainbow.js
@@ -1,15 +1,21 @@
 hardcodeView = {
 	name: "LED 6000 Series TV"
 }
-
-function renderForm(template) {
-	console.log(Mustache.render(template, hardcodeView))
+function tripleRainbowSubmit() {
+	$("#submitform").removeClass("tr-hidden")
 }
 
-function tripleRainbowSubmit() {
-	$.get('tmpl/form.html', renderForm)
+function renderInto(url, view, element, done) {
+	$.get(url, function(data){element.html(Mustache.render(data, view)); done()});
 }
 
 function onLoad() {
-	$('.submit-btn').click(tripleRainbowSubmit)	
+	console.log("loaded!")
+	$('body').append("<div id='submitblock'></div>");
+	renderInto("./tmpl/form.html", hardcodeView, $("#submitblock"), function(){
+		$('#form-closer').click(function(){
+			$('#submitform').addClass("tr-hidden");
+		})
+	});
+	$('.submit-btn').click(tripleRainbowSubmit);
 }
